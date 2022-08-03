c5e9c198d046fca7a1d304290bd0afaf37a63d34
Serhii Bordeniuk <brdnk.srh@gmail.com>
2022-08-03 16:12:30 +0300

    react-helmet add

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
+import {Helmet} from "react-helmet";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`error 404`}
+                />
+                <title>Page doesn't exist</title>
+            </Helmet>
             <ErrorMessage />
             <p
                 style={{
